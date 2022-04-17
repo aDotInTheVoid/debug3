2451f62af80aeb2a591a01edab1695e39a2ac1bc
Nixon Enraght-Moony <nixon.emoony@gmail.com>
2022-04-17 21:48:16 +0100

    Add test for fxhash

---
+++ tests/fxhash.rs
@@ -0,0 +1,29 @@
+
+use debug3::{pprint, Debug};
+use expect_test::{expect, Expect};
+
+fn check(actual: impl Debug, expacted: Expect) {
+    expacted.assert_eq(&pprint(actual));
+}
+
+#[test]
+fn can_use_custom_map() {
+    let mut map = fxhash::FxHashMap::default();
+    check(&map, expect![[r#"{}"#]]);
+    map.insert("a", 1);
+    check(&map, expect![[r#"{"a": 1}"#]]);
+    map.insert("b", 2);
+    // FxHash map is not randomized
+    check(&map, expect![[r#"{"b": 2, "a": 1}"#]]);
+}
+
+#[test]
+fn can_use_custom_hash_set() {
+    let mut set = fxhash::FxHashSet::default();
+    check(&set, expect![[r#"{}"#]]);
+    set.insert(1);
+    check(&set, expect![[r#"{1}"#]]);
+    set.insert(2);
+    // FxHash isn't randomized
+    check(&set, expect![[r#"{1, 2}"#]]);
+}
