e38a9b72bc9c221bbe0d093f8732801a98a2c3d1
Nixon Enraght-Moony <nixon.emoony@gmail.com>
2022-04-18 17:40:53 +0100

    Add SmallVec

--- src/gen_impls/mod.rs
+++ src/gen_impls/mod.rs
@@ -3,4 +3,5 @@ mod hashbrown;
 mod proc_macro2;
 mod rustdoc_types;
 mod serde_json;
+mod smallvec;
 mod syn;
--- src/hand_impls/mod.rs
+++ src/hand_impls/mod.rs
@@ -2,4 +2,5 @@ mod hash_brown;
 mod proc_macro2;
 mod rustdoc_types;
 mod serde_json;
+mod smallvec;
 mod syn;
