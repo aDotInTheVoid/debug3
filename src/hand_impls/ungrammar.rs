98c097a61f9cdd05fa386e54b0710fcd000c1060
Alona Enraght-Moony <code@alona.page>
2023-04-16 18:56:50 +0100

    ungrammar: Nicer repr

--- src/hand_impls/ungrammar.rs
+++ src/hand_impls/ungrammar.rs
@@ -1,3 +1,5 @@
+use ungrammar::{NodeData, TokenData};
+
 impl crate::Debug for ungrammar::Node {
     fn fmt(&self, f: &mut crate::Formatter) {
         // This is a newtyped usize, so this is fine (but I'm not happy about it).
@@ -16,8 +18,11 @@ impl crate::Debug for ungrammar::Grammar {
         struct Nodes<'a>(&'a ungrammar::Grammar);
         impl crate::Debug for Nodes<'_> {
             fn fmt(&self, f: &mut crate::Formatter) {
-                f.debug_list()
-                    .entries(self.0.iter().map(|n| &self.0[n]))
+                f.debug_map()
+                    .entries(self.0.iter().map(|n| {
+                        let NodeData { name, rule } = &self.0[n];
+                        (name, rule)
+                    }))
                     .finish()
             }
         }
@@ -25,8 +30,11 @@ impl crate::Debug for ungrammar::Grammar {
         struct Tokens<'a>(&'a ungrammar::Grammar);
         impl crate::Debug for Tokens<'_> {
             fn fmt(&self, f: &mut crate::Formatter) {
-                f.debug_list()
-                    .entries(self.0.tokens().map(|t| &self.0[t]))
+                f.debug_set()
+                    .entries(self.0.tokens().map(|t| {
+                        let TokenData { name } = &self.0[t];
+                        name
+                    }))
                     .finish()
             }
         }
--- tests/it/ungrammar.rs
+++ tests/it/ungrammar.rs
@@ -24,45 +24,22 @@ ItemList = '{' '}'
     ",
         expect![[r##"
             Grammar {
-                nodes: [
-                    NodeData {
-                        name: "Module",
-                        rule: Seq(
-                            [
-                                Rep(Node(Node(1))),
-                                Opt(Node(Node(2))),
-                                Token(Token(0)),
-                                Node(Node(3)),
-                                Alt([Node(Node(4)), Token(Token(1))]),
-                            ],
-                        ),
-                    },
-                    NodeData {
-                        name: "Attr",
-                        rule: Token(Token(2)),
-                    },
-                    NodeData {
-                        name: "Visibility",
-                        rule: Token(Token(3)),
-                    },
-                    NodeData {
-                        name: "Name",
-                        rule: Token(Token(4)),
-                    },
-                    NodeData {
-                        name: "ItemList",
-                        rule: Seq([Token(Token(5)), Token(Token(6))]),
-                    },
-                ],
-                tokens: [
-                    TokenData { name: "mod" },
-                    TokenData { name: ";" },
-                    TokenData { name: "#[lol]" },
-                    TokenData { name: "pub" },
-                    TokenData { name: "@name" },
-                    TokenData { name: "{" },
-                    TokenData { name: "}" },
-                ],
+                nodes: {
+                    "Module": Seq(
+                        [
+                            Rep(Node(Node(1))),
+                            Opt(Node(Node(2))),
+                            Token(Token(0)),
+                            Node(Node(3)),
+                            Alt([Node(Node(4)), Token(Token(1))]),
+                        ],
+                    ),
+                    "Attr": Token(Token(2)),
+                    "Visibility": Token(Token(3)),
+                    "Name": Token(Token(4)),
+                    "ItemList": Seq([Token(Token(5)), Token(Token(6))]),
+                },
+                tokens: {"mod", ";", "#[lol]", "pub", "@name", "{", "}"},
             }"##]],
     );
 }
